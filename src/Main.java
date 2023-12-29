fd863ef47a2375b81a16fc178e963cecd5c58645
AMahi1998 <68698437+AMahi1998@users.noreply.github.com>
2023-12-28 18:55:40 -0600

    Updated code with Scanner usage

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("hotel booking management!");
-        System.out.println(" Please select your preffered choice\n" +
+        System.out.println("Please select your preferred choice\n" +
                 "\n" +
                 "\t1. register account\n" +
-                "\t2. book reservation \n" +
+                "\t2. book reservation\n" +
                 "\t3. generate bill\n" +
                 "\t4. feedback\n");
+
+        System.out.print("Enter your choice (1-4): ");
+        int userChoice = scanner.nextInt();
+
+        switch (userChoice) {
+            case 1:
+                System.out.println("You selected: Register Account");
+                // Add code for registering account
+                break;
+            case 2:
+                System.out.println("You selected: Book Reservation");
+                // Add code for booking reservation
+                break;
+            case 3:
+                System.out.println("You selected: Generate Bill");
+                // Add code for generating bill
+                break;
+            case 4:
+                System.out.println("You selected: Feedback");
+                // Add code for feedback
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter a number between 1 and 4.");
+        }
+
+        // Don't forget to close the scanner to avoid resource leaks
+        scanner.close();
     }
 }
