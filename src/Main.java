37b338a019091e0beb1811c18a06f528f4db6de6
AMahi1998 <68698437+AMahi1998@users.noreply.github.com>
2023-12-28 18:57:27 -0600

    Updated code with Scanner usage

--- src/Main.java
+++ src/Main.java
@@ -16,25 +16,20 @@ public class Main {
         switch (userChoice) {
             case 1:
                 System.out.println("You selected: Register Account");
-                // Add code for registering account
                 break;
             case 2:
                 System.out.println("You selected: Book Reservation");
-                // Add code for booking reservation
                 break;
             case 3:
                 System.out.println("You selected: Generate Bill");
-                // Add code for generating bill
                 break;
             case 4:
                 System.out.println("You selected: Feedback");
-                // Add code for feedback
                 break;
             default:
                 System.out.println("Invalid choice. Please enter a number between 1 and 4.");
         }
 
-        // Don't forget to close the scanner to avoid resource leaks
         scanner.close();
     }
 }
