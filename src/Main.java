ea8108c5d0ed2bceaf4055e6c0e0920e06786f39
AMahi1998 <68698437+AMahi1998@users.noreply.github.com>
2023-12-28 18:34:31 -0600

    My First Commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("hotel booking management!");
+        System.out.println(" Please select your preffered choice\n" +
+                "\n" +
+                "\t1. register account\n" +
+                "\t2. book reservation \n" +
+                "\t3. generate bill\n" +
+                "\t4. feedback\n");
     }
 }
